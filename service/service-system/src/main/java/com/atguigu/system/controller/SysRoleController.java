f11ed1ffce2258f8d3c5745f77b761ddf0052e7e
1912623646 <1912623646@qq.com>
2022-11-11 17:43:40 +0800

    自定义异常处理测试

--- service/service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
+++ service/service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
@@ -3,6 +3,7 @@ package com.atguigu.system.controller;
 import com.atguigu.common.result.Result;
 import com.atguigu.model.system.SysRole;
 import com.atguigu.model.vo.SysRoleQueryVo;
+import com.atguigu.system.exception.GuiguException;
 import com.atguigu.system.service.SysRoleService;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -28,7 +29,11 @@ public class SysRoleController {
     @GetMapping("/findAll")
     public Result<List<SysRole>> findAll() {
         // TODO 模拟异常
-        int i = 9 / 0;
+        try {
+            int i = 9 / 0;
+        } catch (Exception e) {
+            throw new GuiguException(201, "执行自定义异常处理");
+        }
 
         return Result.ok(sysRoleService.list());
     }
