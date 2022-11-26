cae21508e3187c144779f35e107d5f2b182359f2
jianglinChen191023 <1912623646@qq.com>
2022-11-27 06:41:29 +0800

    系统日志

--- service/service-system/src/main/java/com/atguigu/system/controller/SysOperLogController.java
+++ service/service-system/src/main/java/com/atguigu/system/controller/SysOperLogController.java
@@ -22,7 +22,7 @@ import javax.annotation.Resource;
  * @author 陈江林
  * @date 2022/11/27 06:04
  */
-@Api(tags = "登录日志接口")
+@Api(tags = "系统日志接口")
 @RestController
 @RequestMapping(value = "/admin/system/sysOperLog")
 public class SysOperLogController {
