308d067c10037828a284d887b6f4b340b0432e19
jianglinChen191023 <1912623646@qq.com>
2022-11-28 09:21:29 +0800

    跨域配置

---
+++ service-gateway/src/main/java/com/atguigu/gateway/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.atguigu.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * 跨域配置类
+ *
+ * @author 陈江林
+ * @date 2022/11/28 09:20
+ */
+@Configuration
+public class CorsConfig {
+
+    /**
+     * 处理跨域
+     *
+     * @return {@link CorsWebFilter}
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+
+}
